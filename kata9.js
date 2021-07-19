77de085baaa4da10c9c4a2cc572f8bd9f89ea62b
Leland Mckibben <lmckibben28@hotmail.com>
2021-07-19 21:09:58 +0000

    Created a function that converts a string into camelCase

---
+++ kata9.js
@@ -0,0 +1,37 @@
+/*
+const camelCase = function(input) {
+  let finalString = "";
+
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] !== " ") {
+      finalString += input[i]
+    }else if (input[i] === " ") {
+      let upperCase = input[i + 1].toUpperCase();
+      finalString += upperCase;
+      i++
+    }
+  }
+
+  return finalString;
+};
+*/
+
+
+const camelCase = function(input) {
+  let finalString = "";
+
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] === " ") {
+      let upperCase = input[i + 1].toUpperCase();
+      finalString += upperCase
+      i++
+    }else {
+      finalString += input[i];
+    };
+  };
+  return finalString;
+};
+
+console.log(camelCase("this is a string"));
+console.log(camelCase("loopy lighthouse"));
+console.log(camelCase("supercalifragalisticexpialidocious"));
